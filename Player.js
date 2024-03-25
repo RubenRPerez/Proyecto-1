be56121954fddcf9204e2b2cf415e03799c61991
julio-bh <julio_bh@hotmail.es>
2024-03-25 19:20:19 +0000

    por fin ya rebota por los dos lados y el inferior

--- Player.js
+++ Player.js
@@ -42,41 +42,39 @@ function Player (x, y, board) {
         }
 
 
-
-             if (this.x < platform3.x + platform3.width &&
-                this.x + this.width > platform3.x &&
-                this.y < platform3.y + platform3.height &&
-                this.y + this.height > platform3.y) {
-                    // Verifica si el personaje está tocando el vértice superior de la plataforma
-                    if (this.y + this.height > platform3.y && this.y < platform3.y) {
-                        // Si el personaje está tocando el vértice superior de la plataforma, ajusta su posición y velocidad en consecuencia
-                        this.y = platform3.y - this.height;
-                        this.velocityY = 0;
+if (this.x < platform3.x + platform3.width &&
+    this.x + this.width > platform3.x &&
+    this.y < platform3.y + platform3.height &&
+    this.y + this.height > platform3.y) {
+        // Verifica si el personaje está tocando el vértice superior de la plataforma
+        if (this.y + this.height > platform3.y && this.y + this.height < platform3.y + Math.abs(this.velocityY)) {
+            // Si el personaje está tocando el vértice superior de la plataforma, ajusta su posición y velocidad en consecuencia
+            this.y = platform3.y - this.height;
+            this.velocityY = 0;
         }
-                        // Verifica si el personaje está tocando el vértice inferior de la plataforma
-                    else if (this.y < platform3.y + platform3.height && this.y + this.height > platform3.y + platform3.height) {
-                        // Si el personaje está tocando el vértice inferior de la plataforma, ajusta su posición y velocidad en consecuencia
-                        this.y = platform3.y + platform3.height;
-                        this.velocityY = 0;
-        }
-                        // Verifica si el personaje está tocando los lados de la plataforma
-                    else {
-                        // Verifica si el personaje está chocando con el lado izquierdo de la plataforma
-                    if (this.x + this.width > platform3.x && this.x < platform3.x) {
-                        // Si el personaje está chocando con el lado izquierdo de la plataforma, ajusta su posición en consecuencia
-                        this.x = platform3.x - this.width;
-                }
-                        // Verifica si el personaje está chocando con el lado derecho de la plataforma
-                else if (this.x < platform3.x + platform3.width && this.x + this.width > platform3.x + platform3.width) {
+        // Verifica si el personaje está tocando los lados de la plataforma
+        else {
+            // Verifica si el personaje está chocando con el lado izquierdo de la plataforma
+            if (this.x + this.width > platform3.x && this.x < platform3.x) {
+                // Si el personaje está chocando con el lado izquierdo de la plataforma, ajusta su posición en consecuencia
+                this.x = platform3.x - this.width;
+            }
+            // Verifica si el personaje está chocando con el lado derecho de la plataforma
+            else if (this.x < platform3.x + platform3.width && this.x + this.width > platform3.x + platform3.width) {
                 // Si el personaje está chocando con el lado derecho de la plataforma, ajusta su posición en consecuencia
                 this.x = platform3.x + platform3.width;
             }
+            // Verifica si el personaje está chocando con el lado inferior de la plataforma
+            else if (this.y < platform3.y + platform3.height && this.y > platform3.y) {
+                // Si el personaje está chocando con el lado inferior de la plataforma, ajusta su posición y velocidad en consecuencia
+                this.y = platform3.y + platform3.height;
+                this.velocityY = 0;
+            }
         }
 }
+
    
         
         
     }
 }
-
-
